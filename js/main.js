86a18178b1a4892e44f5fb3b929d9dbdad1c8c2e
host2024 <youwoon5520@gmail.com>
2024-04-17 14:40:48 +0900

    new menu

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 // 운성
 function scrollToElement(selector, block = 'center') {
     const element = document.querySelector(selector);
-    element.scrollIntoView({ behavior: 'smooth', block: block });
+    if (element) {
+        element.scrollIntoView({ behavior: 'smooth', block: block });
+    }
+}
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
 }
 // 운성
